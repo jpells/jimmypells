a6235da51e1dc38a4c0be1bfebb187ef28fa86bc
Jimmy Pells <jimmy@jamespells.com>
2025-04-07 19:49:37 -0400

    :recycle: refactor: formatting & linting via biome (#3)

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,15 +1,17 @@
 const toOxfordComma = (array) =>
   array.length === 2
-    ? array.join(' and ')
+    ? array.join(" and ")
     : array.length > 2
-    ? array
-        .slice(0, array.length - 1)
-        .concat(`and ${array.slice(-1)}`)
-        .join(', ')
-    : array.join(', ');
+      ? array
+          .slice(0, array.length - 1)
+          .concat(`and ${array.slice(-1)}`)
+          .join(", ")
+      : array.join(", ");
 const getProjectTechnology = (project) =>
-  toOxfordComma(project['technologies'].map(function(technology){
-    return '<a href="' + technology["url"] + '" class="hover:underline" target="_blank">' + technology["name"] + '</a>';
-  }));
-const zeroFilled = (n) =>
-  ('0'+n).slice(-1);
+  toOxfordComma(
+    project.technologies.map(
+      (technology) =>
+        `<a href="${technology.url}" class="hover:underline" target="_blank">${technology.name}</a>`,
+    ),
+  );
+const zeroFilled = (n) => ("0" + n).slice(-1);
