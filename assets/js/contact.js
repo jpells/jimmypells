3e9ceb3ab8cb7b2063ef77599f820cc5719c6945
James Pells <jimmy@jamespells.com>
2025-08-30 09:53:18 -0400

    remove manual spam prevention relying solely on recaptcha

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,34 +1,6 @@
-// Initialize variables to track page load time and user's interactions
-const pageLoadTime = Date.now();
-let userInteractionCount = 0;
-
-// Generate token based on page load time
-const sessionToken = btoa(pageLoadTime.toString()).substring(0, 16);
-document.getElementById("token").value = sessionToken;
-
-// Track user's interactions
-document.addEventListener("mousemove", () => userInteractionCount++);
-document.addEventListener("keypress", () => userInteractionCount++);
-document.addEventListener("click", () => userInteractionCount++);
-
 const contactForm = document.getElementById("contact-form");
 contactForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-
-  if (document.getElementById("token").value !== sessionToken) {
-    // Pay the toll!
-    return;
-  }
-
-  if (document.getElementById("website").value) {
-    // Honey do not!
-    return;
-  }
-
-  if (Date.now() - pageLoadTime < 10000 || userInteractionCount < 5) {
-    // Can you do it in less than 10 seconds or 5 moves?
-    return;
-  }
 
   for (let i = 0, len = contactForm.elements.length; i < len; ++i) {
     contactForm.elements[i].disabled = true;
